25b8aaa722911dcb4c8b35503d3fd2b0a359ae49
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-12-10 19:42:57 +0900

    first commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org" 
+gem "sinatra" 
+gem "unicorn" 
+gem 'twitter_oauth'
+gem 'get-twitter-oauth-token'
+gem "rack-flash3"
---
+++ app/app.rb
@@ -0,0 +1,18 @@
+require 'sinatra/base'
+require 'rack/flash'
+
+class App < Sinatra::Base
+
+  set :views, File.dirname(__FILE__) + 'views'
+  set :public_folder, File.dirname(__FILE__) + 'public'
+
+  configure do
+    use Rack::Flash
+    enable :sessions
+  end
+
+  get '/' do
+    'Hello,Sinatra'
+  end
+
+end
