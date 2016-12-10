342c7ab08df5b7eb14407850ea886dd28b5eeb6a
Sa2Knight <shingo.sasaki.0529@gmail.com>
2016-12-10 20:30:17 +0900

    パンカラから持ってきたTwitter.rbから必要なメソッドのみ取り出し

---
+++ app/twitter.rb
@@ -0,0 +1,58 @@
+require 'twitter_oauth'
+class Twitter
+
+  # initialize - usernameでインスタンスを生成する
+  #---------------------------------------------------------------------
+  def initialize(username = nil)
+    key = twitter_api['key']
+    secret = twitter_api['secret']
+    a_token = nil
+    a_secret = nil
+
+    if username
+      if @access_token = Util.read_secret(@username)
+        a_token = @access_token[:token] || nil
+        a_secret = @access_token[:secret] || nil
+      end
+      @twitter = TwitterOAuth::Client.new(
+        :consumer_key => key,
+        :consumer_secret => secret,
+        :token => a_token,
+        :secret => a_secret
+      )
+      @authed = @twitter && @twitter.info['screen_name'] ? true : false
+    end
+  end
+
+  # request_token - Twitter認証用のURLを生成する
+  #--------------------------------------------------------------------
+  def request_token(callback)
+    request_token = @twitter.request_token(:oauth_callback => callback)
+    return request_token
+  end
+
+  # get_access_token - Twitter連携用のアクセストークンを取得する
+  #--------------------------------------------------------------------
+  def get_access_token(req_token , req_secret , verifier)
+    token = @twitter.authorize(req_token , req_secret , :oauth_verifier => verifier)
+    Util.write_secret(@username , {
+      :token => token.token ,
+      :secret => token.secret ,
+      :username => @twitter.info['screen_name'] ,
+      :icon => @twitter.info['profile_image_url']
+    })
+  end
+
+  # username - ユーザ名を取得する
+  #--------------------------------------------------------------------
+  def username
+    @authed and return @twitter.info['screen_name']
+  end
+
+  # icon - ユーザのアイコンを取得する
+  #--------------------------------------------------------------------
+  def icon
+    @authed and return @twitter.info['profile_image_url']
+  end
+
+end
